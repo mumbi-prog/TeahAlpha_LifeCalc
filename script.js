e371e1ccfc929c4d15328a1647e37004dd2ffbc6
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-03 13:58:15 +0300

    Added a validation to ensure only past dates are added

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ function calculateAge() {
     let dobInput = document.getElementById('dateofbirth').value;
     let mydateofbirth = new Date(dobInput);
     let today = new Date();
+
+        if (mydateofbirth > today) {
+            alert("Please add today's date or one in the past.");
+            return;
+        }
+  
 }
