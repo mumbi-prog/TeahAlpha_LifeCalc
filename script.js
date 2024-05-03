80fa70cd56f16bb25b887061186dff80efbb4d8b
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-03 14:25:00 +0300

    Calculated milliseconds into yrs, months and days

--- script.js
+++ script.js
@@ -9,5 +9,10 @@ function calculateAge() {
         }
 
         const ageInMilliseconds = today.getTime() - mydateofbirth.getTime();
-    
+        const millisecondsinaday = 1000 * 60 * 60 * 24
+        const ageInDays = Math.floor(ageInMilliseconds / millisecondsinaday);
+        const years = Math.floor(ageInDays / 365);
+        const months = Math.floor((ageInDays % 365) / 30);
+        const days = ageInDays % 30;
+     
 }
